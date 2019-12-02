78a3f81836fab017a734e3b798d7de980a5af349
qwj <1286628397@qq.com>
2019-12-02 16:24:47 +0800

    init repo

---
+++ src/test/java/com/qwj/commumitytwo/CommumitytwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwj.commumitytwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommumitytwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
